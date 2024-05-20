e50cc5a74aaade6eefc3a5206777ae7e371203fa
MichaelBosciglio <55165965+MichaelBosciglio@users.noreply.github.com>
2024-05-20 16:49:26 -0400

    Added Tailwind / defaults

--- app.py
+++ app.py
@@ -1,35 +1,48 @@
 from flask import Flask, render_template, request
 from bs4 import BeautifulSoup
 import requests
-import re
 
 app = Flask(__name__, template_folder='templates')
 
 def scrape_player_info(player_name):
     # Split the player name into first and last names
     parts = player_name.split()
-    first_name = parts[0].lower()
-    last_name = parts[1].lower()
     
-    print(first_name, last_name)
-    # Construct the player's URL
-    player_url = f'https://www.basketball-reference.com/players/{last_name[0]}/{last_name[:5]}{first_name[:min(2, len(first_name))]}01.html'
-    print("Player URL:", player_url)
+    # Check if the player name has at least two parts
+    if len(parts) < 2:
+        return ['607', '6.6', '5.5', '0.9', '49.2', '11.1', '57.0', '49.2', 'Kwame Brown', 'https://www.basketball-reference.com/req/202106291/images/headshots/brownkw01.jpg', True]
+    else:
+        first_name = parts[0].lower()
+        last_name = parts[1].lower()
+        
+        print(first_name, last_name)
+        # Construct the player's URL
+        player_url = f'https://www.basketball-reference.com/players/{last_name[0]}/{last_name[:5]}{first_name[:min(2, len(first_name))]}01.html'
+        print("Player URL:", player_url)
 
-    # Send a request to the player's URL
-    response = requests.get(player_url)
-    print("Response Status Code:", response.status_code)
+        # Send a request to the player's URL
+        response = requests.get(player_url)
+        print("Response Status Code:", response.status_code)
 
     if response.status_code == 200:
         # Parse the HTML content
         soup = BeautifulSoup(response.content, 'html.parser')
-        face = soup.find(class_="media-item").find('img')['src']
-        stats = soup.find_all(class_="stats_pullout")
+        media_item = soup.find(class_="media-item")
         
-        for i in stats:
-            stats=i.text.strip().replace(' ', '').split('\n')
+        if not media_item:
+            return ['607', '6.6', '5.5', '0.9', '49.2', '11.1', '57.0', '49.2', 'Kwame Brown', 'https://www.basketball-reference.com/req/202106291/images/headshots/brownkw01.jpg', True]
 
-        career_stats = [0, 0, 0, 0, 0, 0, 0, 0, player_name, face]
+        face = media_item.find('img')['src']
+        print(face)
+        stats_section = soup.find_all(class_="stats_pullout")
+
+        if not stats_section:
+            return ['607', '6.6', '5.5', '0.9', '49.2', '11.1', '57.0', '49.2', 'Kwame Brown', 'https://www.basketball-reference.com/req/202106291/images/headshots/brownkw01.jpg', True]
+        
+        for stats in stats_section:
+            stats = stats.text.strip().replace(' ', '').split('\n')
+        
+        career_stats = [0, 0, 0, 0, 0, 0, 0, 0, player_name, face, False]
         
         for i in range(8):
             if i < 4:
@@ -38,16 +51,16 @@ def scrape_player_info(player_name):
                 career_stats[i] = stats[(i*3)+8]
         
         print(career_stats)
+    else:
+        return ['607', '6.6', '5.5', '0.9', '49.2', '11.1', '57.0', '49.2', 'Kwame Brown', 'https://www.basketball-reference.com/req/202106291/images/headshots/brownkw01.jpg', True]
 
     return career_stats
-    
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     player_info = None
     if request.method == 'POST':
-        player_name = request.form['player_name']
-        
+        player_name = request.form['player_name'].strip()
         player_info = scrape_player_info(player_name)
     
     return render_template('index.html', player_info=player_info)
